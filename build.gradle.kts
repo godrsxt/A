25fc26120ba66b2087033ede0d4756984c04223f
godrsxt <rushabgshitre360@gmail.com>
2026-07-19 05:51:43 +0530

    Auto-commit: build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
